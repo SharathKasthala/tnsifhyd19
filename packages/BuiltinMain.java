fa0f9acc3d558d0d2c86461a159104d3ee43ef66
SharathKasthala <150805728+SharathKasthala@users.noreply.github.com>
2024-05-03 22:52:50 +0530

    Add files via upload

---
+++ packages/BuiltinMain.java
@@ -0,0 +1,13 @@
+package packages;
+import java.lang.Math.*;
+
+public class BuiltinMain {
+	public static void main(String[] args) {
+		int a=100;
+		int b=98;
+		int c=Math.max(a, b);
+		System.out.println(c);
+		
+	}
+
+}
